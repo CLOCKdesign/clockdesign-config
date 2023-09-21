4c1566f652c43ce8586463e9d18a6079c70bf9fb
Marco Niederberger <marco@ni-m.ch>
2023-09-21 21:33:10 +0200

    Toggle buttons on connection

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 [![Deploy to FTP server](https://github.com/CLOCKdesign/clockdesign-config/actions/workflows/main.yaml/badge.svg)](https://github.com/CLOCKdesign/clockdesign-config/actions/workflows/main.yaml)
 
 A minimalistic webpage with WebBLE
+
+The root index.html contains a simple interface for the enduser.
+Enter the debug mode by adding <code>?debug</code> to the end of the url.
+
+### Technical details
+- The BLE part is based on the javascript WebBLE API, as documented in [developer.mozilla](https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API), [novelbits.io](https://novelbits.io/web-bluetooth-getting-started-guide/) and [developer.chrome](https://developer.chrome.com/articles/bluetooth/)
+- The site gets pushed to my FTP server on every git push. On the <code>dev</code> branch it is published to [dev.clockdesign.ch](https://dev.clockdesign.ch) and on the <code>main</code> branch to [config.clockdesign.ch](https://config.clockdesign.ch).
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 // https://novelbits.io/web-bluetooth-getting-started-guide/
 // https://developer.mozilla.org/en-US/docs/Web/API/BluetoothRemoteGATTCharacteristic/readValue
+// https://developer.chrome.com/articles/bluetooth/
 
 const deviceService = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 const deviceTXcharacteristic = '6e400002-b5a3-f393-e0a9-e50e24dcca9e'; // send to ESP32
@@ -16,42 +17,48 @@ const btnChangeModi = document.getElementById("btnChangeModi");
 btnUpdateTime.addEventListener("click", sendNewTime);
 
 // buttons on debug
-const btnConnect = document.getElementById("connect");
+const btnToggleConnection = document.getElementById("toggleConnection");
 const btnSend0x30 = document.getElementById("send0x30");
 const btnPrintTime = document.getElementById("printTime");
 const btnStrandtest = document.getElementById("stateStrandtest");
 const btnMatrix = document.getElementById("stateMatrix");
 const btnWordclock = document.getElementById("stateWordclock");
 
-btnConnect.addEventListener("click", connectToCLOCK);
+btnToggleConnection.addEventListener("click", connectToCLOCK);
 btnSend0x30.addEventListener("click", send0x30);
 btnPrintTime.addEventListener("click", sendTime);
 btnStrandtest.addEventListener("click", sendStrandtest);
 btnMatrix.addEventListener("click", sendMatrix);
 btnWordclock.addEventListener("click", sendWordclock);
 
-async function connectToCLOCK() {
-    connectionStatus.textContent = "Wordclock wird gesucht...";
+async function connectToCLOCK(force = false) {
     try {
-        device = await navigator.bluetooth.requestDevice({
-            filters: [
-                { namePrefix: 'CLOCKdesign' }
-            ],
-            optionalServices: [deviceService]
-        });
-        console.log(device);
-        connectedDevice = await device.gatt.connect();
-        connectionStatus.textContent = "CONNECTED";
-        const espService = await connectedDevice.getPrimaryService(deviceService);
-        connectionStatus.textContent = "UART RX connected";
-        const uartRXCharacteristic = await espService.getCharacteristic(deviceRXcharacteristic);
-        uartTXCharacteristic = await espService.getCharacteristic(deviceTXcharacteristic);
-        connectionStatus.textContent = "UART RX characteristic discovered";
-        const uartRXtext = await uartRXCharacteristic.startNotifications()
-        console.log(uartRXtext);
-        const eventRXlistener = await uartRXCharacteristic.addEventListener('characteristicvaluechanged', handleNotifications);
-
-    } catch (error) {
+        toggleState();
+        if (!connectedDevice || !force) {
+            connectionStatus.textContent = "Wordclock wird gesucht...";
+            device = await navigator.bluetooth.requestDevice({
+                filters: [
+                    { namePrefix: 'CLOCKdesign' }
+                ],
+                optionalServices: [deviceService]
+            });
+            console.log(device);
+            connectedDevice = await device.gatt.connect();
+            connectionStatus.textContent = "CONNECTED";
+            const espService = await connectedDevice.getPrimaryService(deviceService);
+            connectionStatus.textContent = "UART RX connected";
+            const uartRXCharacteristic = await espService.getCharacteristic(deviceRXcharacteristic);
+            uartTXCharacteristic = await espService.getCharacteristic(deviceTXcharacteristic);
+            connectionStatus.textContent = "UART RX characteristic discovered";
+            const uartRXtext = await uartRXCharacteristic.startNotifications()
+            console.log(uartRXtext);
+            const eventRXlistener = await uartRXCharacteristic.addEventListener('characteristicvaluechanged', handleNotifications);
+        }
+        else {
+            disconnect();
+        }
+    }
+    catch (error) {
         if (typeof device !== 'undefined') {
             connectionStatus.textContent = "CONNECTION FAILED";
         }
@@ -59,6 +66,9 @@ async function connectToCLOCK() {
             connectionStatus.textContent = "CANCELLED"
         }
         console.log(String(error));
+    }
+    finally {
+        toggleState();
     }
 
 }
@@ -129,15 +139,21 @@ function sendWordclock() {
     }
 }
 
+async function disconnect() {
+    setTimeout(async function () {
+        connectedDevice = await device.gatt.disconnect();
+        console.log("Disconnected");
+        connectionStatus.textContent = "Disconnected"
+        toggleState();
+    }, 300);
+}
+
 async function sendNewTime() {
-    await connectToCLOCK();
+    await connectToCLOCK(force = true);
     if (device) {
         extractTime();
         alert("Uhrzeit erfolgreich aktualisiert");
-        setTimeout(async function () {
-            await device.gatt.disconnect();
-            console.log("Disconnected");
-        }, 300);
+        disconnect();
     }
 }
 
@@ -168,19 +184,36 @@ function extractTime() {
     console.log(packetArray);
 }
 
-setTimeout(function () {
-    const currentUrl = window.location.href;
-    if(currentUrl.endsWith('debug')) {
-        var collection = document.getElementsByClassName('debugOnly');
-        for (let el of collection) {
-        el.style.display  = "block";
-        }
-    //   if (x.style.display === "none") {
-    //   } else {
-    //     x.style.display = "none";
-    //   }
-    console.log(currentUrl);
-    console.log(currentUrl.endsWith('debug'));
+function toggleState() {
+    var button = document.getElementById("toggleConnection");
+    if (connectedDevice) {
+        button.style.backgroundColor = "green";
     }
-}, 300);
+    else {
+        button.style.backgroundColor = "red";
+    }
+    var disabled;
+    var color;
+    if (connectedDevice) {
+        disabled = false;
+        color = "black"
+    } else {
+        disabled = true;
+        color = "gray"
+    }
+    var buttons = document.getElementsByClassName("onConnOnly");
+    for (let el of buttons) {
+        el.disabled = disabled;
+        el.style.backgroundColor = color;
+    }
+}
 
+toggleState();
+
+const currentUrl = window.location.href;
+if (currentUrl.endsWith('debug')) {
+    var collection = document.getElementsByClassName('debugOnly');
+    for (let el of collection) {
+        el.style.display = "block";
+    }
+}
