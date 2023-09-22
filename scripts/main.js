28e6aa4b25129f1c83184f94a824357ce471167d
Marco Niederberger <marco@ni-m.ch>
2023-09-23 00:52:57 +0200

    Add GPLv3 licence

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,33 @@
-// https://novelbits.io/web-bluetooth-getting-started-guide/
-// https://developer.mozilla.org/en-US/docs/Web/API/BluetoothRemoteGATTCharacteristic/readValue
-// https://developer.chrome.com/articles/bluetooth/
+/******************************************************************************
+ * Copyright (C) 2023 by Niederberger Marco                                   *
+ *                                                                            *
+ * This file is part of config.CLOCKdesign.                                   *
+ *                                                                            *
+ * config.CLOCKdesign is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * config.CLOCKdesign is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with linescaleGUI. If not, see <http://www.gnu.org/licenses/>.       *
+ ******************************************************************************/
+/**
+ * @file main.js
+ * @authors Niederberger Marco
+ *
+ * @brief The javascript file for the config site
+ * It contains all BLE functionality's.
+ * 
+ * @see
+ * https://novelbits.io/web-bluetooth-getting-started-guide/
+ * https://developer.mozilla.org/en-US/docs/Web/API/BluetoothRemoteGATTCharacteristic/readValue
+ * https://developer.chrome.com/articles/bluetooth/
+*/
 
 const deviceService = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 const deviceTXcharacteristic = '6e400002-b5a3-f393-e0a9-e50e24dcca9e'; // send to ESP32
